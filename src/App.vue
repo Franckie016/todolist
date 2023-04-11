a80b3b1ab22658f7a7587a3e0c93ad231f77832b
Franckie <voninjatovofranckie@gmail.com>
2023-04-11 11:10:19 +0000

    redesign 1.2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Todos from '@/components/Todos.vue'
-  import "./assets/styles/libs/reset.css"
+  import "./assets/styles/libs/reset.scss"
+  
 </script>
 
 <template>
@@ -10,8 +11,9 @@
 </template>
 
 
-<style>
+<style lang="scss">
+@import "./assets/styles/libs/allColors.scss";
 body {
-  background-color: #edf2f4;
+  background-color: $color-primary;
 }
 </style>
